bdd6f84679a88945236c4db3f20532462eabc4e4
Gilke Vermander <gilke.vermander@student.howest.be>
2021-02-04 22:26:16 +0100

    kranten waaien + animatie voorblad + footstep

--- BAP4/Assets/scripts/ARTapToPlaceObject.cs
+++ BAP4/Assets/scripts/ARTapToPlaceObject.cs
@@ -10,12 +10,23 @@ public class ARTapToPlaceObject : MonoBehaviour
 {
     public GameObject objectToPlace;
     public GameObject placementIndicator;
+    public GameObject foot;
+    public GameObject krantWrap;
+    public GameObject voorblad;
+
+    public float easing = 2f;
+
+    private bool touched;
 
     private Animator doosOpenen;
+    private Animator krantMetComments;
 
     private ARSessionOrigin arOrigin;
 
     private Pose placementPose;
+
+    Vector3 footPos;
+    Vector3 krantPos;
 
     private bool placementPoseIsValid = false;
 
@@ -23,19 +34,25 @@ public class ARTapToPlaceObject : MonoBehaviour
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
         doosOpenen = objectToPlace.GetComponentInChildren<Animator>();
-        
-        //doosOpenen.SetBool("start", true);
-        //Debug.Log(objectToPlace.activeSelf);
-        //Debug.Log(doosOpenen.GetBool("start"));
+        krantMetComments = voorblad.GetComponentInChildren<Animator>();
+        touched = false;
+        foot.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        krantWrap.SetActive(false);
+        voorblad.transform.localScale = new Vector3(0f, 0f, 0f);
+
     }
 
     void Update()
     {
         UpdatePlacementPose();
         UpdatePlacementIndicator();
+        
 
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (placementPoseIsValid && Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began && touched == false)
         {
+            touched = true; //ervoorzorgen dat je maar 1 keer kan duwen
+            Debug.Log(touched);
+            placementIndicator.transform.localScale = new Vector3(0f, 0f, 0f); //indicator onzichtbaar maken
             PlaceObject();
         }
     }
@@ -43,8 +60,42 @@ public class ARTapToPlaceObject : MonoBehaviour
     private void PlaceObject()
     {
         Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
-        //animatie starten
+        //AR starten
+        StartCoroutine(ArExpierence());
+    }
+
+    private IEnumerator ArExpierence()
+    {
+        //doos openen
+        
         doosOpenen.Play("doos openen");
+
+        yield return new WaitForSeconds(3); //doos is volledig geopend.
+        //footstep tevoorschijn laten komen 1 unit naast de getrackte positie.
+        footPos = new Vector3(placementPose.position.x + 1.0f, placementPose.position.y, placementPose.position.z);
+        krantPos = new Vector3(placementPose.position.x + 1.0f, placementPose.position.y + 1.0f, placementPose.position.z);
+        Instantiate(foot, footPos, placementPose.rotation);
+        
+        yield return new WaitForSeconds(2);
+        //audio starten
+        //kranten komen tevoorschijn en beginnen te wapperen op de plaats waar de doos staat!
+        krantWrap.transform.localScale = new Vector3(1f, 1f, 1f);
+        krantWrap.SetActive(true);
+        StartCoroutine(KrantenVliegen(placementPose.position, krantPos, easing, krantWrap));
+        voorblad.transform.localScale = new Vector3(1f, 1f, 1f);
+        krantMetComments.Play("krantMetComments");
+
+    }
+
+    IEnumerator KrantenVliegen(Vector3 krant_startpos, Vector3 krant_endpos, float seconds, GameObject krant)
+    {
+        float t = 0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            krant.GetComponent<Transform>().localPosition = Vector3.Lerp(krant_startpos, krant_endpos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
     }
 
     private void UpdatePlacementIndicator()
